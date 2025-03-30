19e438112a85341ca017afea7d183da045c7a17f
Ro0kly <snallytango@yandex.ru>
2025-03-30 23:56:10 +0300

    main / implement Vector class

---
+++ src/main.cc
@@ -0,0 +1,13 @@
+#include <cstddef>
+#include <iostream>
+
+template <typename T> class Vector {
+private:
+  size_t size_;
+  size_t capacity_;
+  T *data;
+};
+int main() {
+  int a = 0;
+  return 0;
+}
